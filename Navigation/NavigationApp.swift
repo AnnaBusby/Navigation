7e562e01d26e3f2f34f65fd417d8bc77f61c3331
Anna Busby <anna.busby.services@outlook.com>
2024-01-14 11:26:25 +0000

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Anna Busby on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
